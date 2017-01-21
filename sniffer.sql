3b2cab633a91e0351e4730e7d641bd62815bb78d
AlinaIshtuganova5 <dishtuganova@gmail.com>
2017-01-21 22:38:02 +0300

    Update sniffer.sql

--- sniffer.sql
+++ sniffer.sql
@@ -1,10 +1,4 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
-CREATE DATABASE IF NOT EXISTS sniffer /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS sniffer;
 USE `sniffer`;
 
 CREATE TABLE IF NOT EXISTS sniffresult (
@@ -39,10 +33,9 @@ CREATE TABLE IF NOT EXISTS databasechangelog (
 );
 
 
-/*!40000 ALTER TABLE databasechangelog DISABLE KEYS */;
+
 INSERT INTO databasechangelog (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, `EXECTYPE`, MD5SUM, DESCRIPTION, COMMENTS, TAG, LIQUIBASE, CONTEXTS, LABELS) VALUES
   ('1476220990929-1', 'eljah32 (generated)', 'src/main/resources/liquibase/v-1.0/changelog-v.1.0-init-11-10-2016.xml', '2016-10-19 22:56:27', 1, 'EXECUTED', '7:5da484f31121e22b65a7479f1cb396b9', 'createTable', '', NULL, '3.4.1', NULL, NULL);
-/*!40000 ALTER TABLE databasechangelog ENABLE KEYS */;
 
 
 
@@ -55,16 +48,9 @@ CREATE TABLE IF NOT EXISTS databasechangeloglock (
 );
 
 
-/*!40000 ALTER TABLE `databasechangeloglock` DISABLE KEYS */;
 INSERT INTO `databasechangeloglock` (ID, LOCKED, LOCKGRANTED, LOCKEDBY) VALUES
   (1, b'0', NULL, NULL);
-/*!40000 ALTER TABLE databasechangeloglock ENABLE KEYS */;
 
-
-
-
-
-/*!40000 ALTER TABLE sniffresult DISABLE KEYS */;
 INSERT INTO `sniffresult` (id, referer, hijackedurl, author, sniffdate) VALUES
   (1, NULL, NULL, 'user', '2017-01-20 20:45:26.000000'),
   (2, NULL, NULL, 'hacker', '2017-01-20 20:45:48.000000'),
@@ -72,21 +58,13 @@ INSERT INTO `sniffresult` (id, referer, hijackedurl, author, sniffdate) VALUES
   (4, NULL, NULL, 'hacker1', '2017-01-20 20:46:20.000000'),
   (5, NULL, NULL, 'admin', '2017-01-20 20:46:45.000000'),
   (6, NULL, NULL, 'hacker', '2017-01-20 20:59:26.000000');
-/*!40000 ALTER TABLE `sniffresult` ENABLE KEYS */;
 
 
-
-
-
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`user_name`, `password`) VALUES
   ('user', '9ef8295458b745af7a392ab23802ba7b'),
   ('hacker1', '1b2b4c6643b5c96a07abdf16fb2220fe'),
   ('hacker', 'b9049640fdab0fea20d2af6ac2965405'),
   ('user1', '24c9e15e52afc47c225b757e7bee1f9d');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
-
 
 CREATE TABLE IF NOT EXISTS `user_roles` (
   `user_name` varchar(20) NOT NULL,
@@ -95,13 +73,9 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
 INSERT INTO `user_roles` (`user_name`, `role_name`) VALUES
   ('hacker', 'hacker'),
   ('hacker1', 'hacker'),
   ('user', 'admin'),
   ('user1', 'admin');
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
